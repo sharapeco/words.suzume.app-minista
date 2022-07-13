d6d4c910d8717f1a4fed68d7a8a65a9b7dd1ff0d
Shinichiro Yabu <scin.ichi@gmail.com>
2022-07-13 17:15:18 +0900

    🐤

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { Head } from "minista"
+
+const PageHome = () => {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <h1>Hello!</h1>
+    </>
+  )
+}
+
+export default PageHome
