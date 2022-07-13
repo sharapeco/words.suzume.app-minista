a1de6d284801103f9d2b803b9b314965dc185a87
Shinichiro Yabu <scin.ichi@gmail.com>
2022-07-13 17:22:30 +0900

    Format codes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import { Head } from "minista"
+import { Head } from 'minista'
 
 const PageHome = () => {
-  return (
-    <>
-      <Head>
-        <title>Home</title>
-      </Head>
-      <h1>Hello!</h1>
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<title>Home</title>
+			</Head>
+			<h1>Hello!</h1>
+		</>
+	)
 }
 
 export default PageHome
