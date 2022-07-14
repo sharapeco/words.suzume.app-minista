e354507f68c9f81bbd957f4c78f92b3c02fffdc6
Shinichiro Yabu <scin.ichi@gmail.com>
2022-07-14 17:19:59 +0900

    Format

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -14,11 +14,15 @@ export default function Page404() {
 				</nav>
 
 				<article className="word">
-					<header className='word-header'>
-						<h1 className='word-name'>見つかりません</h1>
+					<header className="word-header">
+						<h1 className="word-name">見つかりません</h1>
 					</header>
-					<p className="word-content">このページは移動もしくは削除された可能性があります。</p>
-					<p className="word-content"><a href='/'>索引</a>から探してみてください。</p>
+					<p className="word-content">
+						このページは移動もしくは削除された可能性があります。
+					</p>
+					<p className="word-content">
+						<a href="/">索引</a>から探してみてください。
+					</p>
 				</article>
 			</div>
 		</>
