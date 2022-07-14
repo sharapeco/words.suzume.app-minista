02c6628dd218636f09608b709a99ae8fb3a35ee8
Shinichiro Yabu <scin.ichi@gmail.com>
2022-07-14 17:19:50 +0900

    Add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,26 @@
+import { Head } from 'minista'
+import './word.css'
+
+export default function Page404() {
+	return (
+		<>
+			<Head>
+				<title>見つかりません</title>
+			</Head>
+
+			<div className="container">
+				<nav className="nav">
+					<a href="/">ない言葉</a>
+				</nav>
+
+				<article className="word">
+					<header className='word-header'>
+						<h1 className='word-name'>見つかりません</h1>
+					</header>
+					<p className="word-content">このページは移動もしくは削除された可能性があります。</p>
+					<p className="word-content"><a href='/'>索引</a>から探してみてください。</p>
+				</article>
+			</div>
+		</>
+	)
+}
